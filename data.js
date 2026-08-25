2b97ea0d6ef0722daf5387c05226ac007232e070
Eryza <blackclouds3456@gmail.com>
2026-08-25 10:08:20 +0700

    Update data.js

--- data.js
+++ data.js
@@ -136,7 +136,7 @@ const MOVIES = [
     description: "Rewind Minecraft 2025 Sinopsis mengisahkan petualangan Mark, Freya, dan Benny di 2026. Mereka akan melanjutkan kisah yang telah dimulai di 2025 dengan berbagai tantangan dan keajaiban yang mereka temukan.",
     poster: "https://placehold.co/400x600/047857/ffffff?text=On+Street+%26+Dirt",
     banner: "rmcib2025.jpg",
-    video: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4",
+    video: "https://www.youtube.com/watch?v=NJU98xmziS4",
     subtitle: "subtitles/rmci2025.vtt",
     category: "Animasi"
   }
